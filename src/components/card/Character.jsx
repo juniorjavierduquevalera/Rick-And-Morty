3de14305f6fed0328d5920a9eda08b861637756b
Steven Varela <stevenvarela98@gmail.com>
2023-02-03 19:19:16 -0400

    :wrench: character lazy loading

--- src/components/card/Character.jsx
+++ src/components/card/Character.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 import './styles.css'
 
 const Character = (character) => {
   return (
       <div className="card w-80 bg-base-100 shadow-2xl mx-10 my-5 card-contain">
-          <figure className="px-10 pt-10">
+        <LazyLoadImage
+          alt={character.name}
+          effect="blur"
+          // height={image.height}
+          src={character.image} // use normal <img> attributes as props
+          // width={image.width}
+        />
+          {/* <figure className="px-10 pt-10">
             <img src={character.image} alt={character.name} className="rounded-md" />
-          </figure>
+          </figure> */}
         <div className="card-body items-center">
           <h2 className="card-title text-center">{character.name}</h2>
           <ul className="block text-left">
