6d840b8129aa6e0778ccf1d4f012945a0febdc75
Steven Varela <stevenvarela98@gmail.com>
2023-02-03 19:12:17 -0400

    :hammer: infinite scroll - component

--- src/components/CharactersList.jsx
+++ src/components/CharactersList.jsx
@@ -3,48 +3,66 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import Character from './card/Character'
 import NavPage from './NavPage'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 
 const CharactersList = () => {
     const [characters, setCharacters] = useState([]);
     const [loading, setLoading] = useState(true);
     const [page, setPage] = useState(1);
+    const [apiInfo, setApiInfo] = useState({count: 1, pages: 1, next: '', prev: ''});
 
     const getApi = async () => {
-        const response = await axios.get(`https://rickandmortyapi.com/api/character?page=${page}`);
-        setCharacters(response.data.results);
-        setLoading(false);
-        console.log(response.data.results);
+      const response = await axios.get(`https://rickandmortyapi.com/api/character?page=${page}`);
+      setCharacters([...characters, ...response.data.results]);
+      console.log('>>: characters > ', characters)
+      setLoading(false);
+      setApiInfo(response.data.info)
+      console.log(response.data.results);
     };
 
     useEffect(() => {
-        getApi()
+      getApi()
     }, [page]);
 
-    if (loading) {
-      return (
-        <div className='flex items-center justify-center h-screen'>loading...</div>
-      )
+    // if (loading) {
+    //   return (
+    //     <div className='flex items-center justify-center h-screen'>loading...</div>
+    //   )
+    // }
+
+    const handlePageAndData = () => {
+      if(page+1 <= apiInfo.pages)
+        setPage(page+1);
     }
+
+    console.log('>>: pages > ', page < (apiInfo.pages+1))
 
     return (
       <div>
         <h1 className='text-green-500 text-4xl font-bold text-center mt-10'>Rick and Morty</h1>
-        <NavPage ActualPage={page} setPage={setPage} />
-        <div className="flex flex-wrap justify-center">
-              {characters.map((character) => (
-                <div className="" key={character.id}>
-                  <Character
-                    key={character.id}
-                    name={character.name}
-                    origin={character.origin}
-                    image={character.image}
-                    species={character.species}
-                  />
-                </div>
-              ))}
-        </div>
-        <NavPage ActualPage={page} setPage={setPage} />
+        {/* <NavPage ActualPage={page} setPage={setPage} /> */}
+        <InfiniteScroll
+          dataLength={characters.length}
+          next={handlePageAndData}
+          hasMore={page < (apiInfo.pages+1)}
+          loader={<h4>Loading...</h4>}
+        >
+          <div className="flex flex-wrap justify-center">
+            {characters.map((character) => (
+              <div className="" key={character.id.toString()}>
+                <Character
+                  key={character.id.toString()}
+                  name={character.name}
+                  origin={character.origin}
+                  image={character.image}
+                  species={character.species}
+                />
+              </div>
+            ))}
+          </div>
+        </InfiniteScroll>
+        {/* <NavPage ActualPage={page} setPage={setPage} /> */}
       </div>
       );
     }
